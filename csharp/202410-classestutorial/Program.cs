d2b819caca7f272ae49ffc5319a3090c1c17b370
kodex <lorenzharfst@gmail.com>
2024-12-18 11:24:47 +0100

    Fix re-throwing a caugh exception warning

--- csharp/202410-classestutorial/Program.cs
+++ csharp/202410-classestutorial/Program.cs
@@ -6,13 +6,13 @@ public class Program
     public static void Main(string[] args)
     {
         try {
-        BankAccount myBankAccount = new BankAccount("Lorentius", 200);
+        BankAccount myBankAccount = new BankAccount("Lorentius", -200);
         Console.WriteLine($"Succesfully created Bank Account for: {myBankAccount.Owner}, with balance: {myBankAccount.Balance}");
         } catch (Exception ex)
         {
             if (ex is NegativeBalanceException)
             {
-                Console.WriteLine("cum");
+                Console.WriteLine(ex.Message);
             }
             else
             {
