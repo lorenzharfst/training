c486c403057adb232dd6c23f8c46b1159e45d870
kodex <lorenzharfst@gmail.com>
2024-12-20 13:33:24 +0100

    Initialise Java sandbox project

---
+++ java/202410-sandbox/SandboxClass.java
@@ -0,0 +1,9 @@
+public class SandboxClass {
+    public static void main(String[] args) {
+        String[] nameArray = {"Joe", "Jim", "Mike Jordan"};
+        System.out.println("Welcome to the Sandbox.");
+        for (String name : nameArray) {
+            System.out.println(name);
+        }
+    }
+}
