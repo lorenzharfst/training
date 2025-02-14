d4f7bc47461b7ed15e102b9e09f19d99ac6c1437
Lorenz Harfst <lorenzharfst@gmail.com>
2025-02-14 12:14:48 +0100

    Add Collection examples

--- java/202501-mavensandbox/src/main/java/com/mycompany/app/Iterables.java
+++ java/202501-mavensandbox/src/main/java/com/mycompany/app/Iterables.java
@@ -1,6 +1,8 @@
 package com.mycompany.app;
 
 import java.util.List;
+import java.util.Collection;
+import java.util.ArrayList;
 
 public class Iterables<T> {
     List<T> list;
@@ -13,5 +15,24 @@ public class Iterables<T> {
         for (T element: list){
             System.out.println(element);
         }
+        System.out.println("Test Collection instantiating ArrayList");
+        Collection<String> col = new ArrayList<>();
+        col.add("Herro");
+        col.add("Hiya");
+        System.out.println(col);
+        Collection<String> col2 = new ArrayList<>();
+        col2.add("Herro");
+        col2.add("Hey");
+        System.out.println("Does col contain everything in col2? " + col.containsAll(col2));
+        System.out.println("Adding all elements of col2 into col");
+        col.addAll(col2);
+        System.out.println("Does col contain everything in col2? " + col.containsAll(col2));
+        System.out.println("Collection col: " + col);
+        System.out.println("Copying to an array with toArray(), using a 0 length array since it will automatically create a correct size and return that");
+        String[] resultArray = col.toArray(String[]::new);
+        System.out.println("Attempting to create a List out of an array");
+        String[] stringArray = { "stringEl1", "stringEl2", "stringEl3" };
+        List<String> stringList = List.of(stringArray);
+        System.out.println("Resulting List " + stringList);
     }
 }
